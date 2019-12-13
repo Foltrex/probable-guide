f8041bb1e3094c7d43314719fbf3c212fbc8964e
Kalaputs, Andrei <akalaputs@scnsoft.com>
2019-12-13 19:23:05 +0300

    Updated function validation (without history).

--- scn-worklog-plugin/src/main/java/com/scn/jira/jql/function/UpdatedWithoutHistoryFunction.java
+++ scn-worklog-plugin/src/main/java/com/scn/jira/jql/function/UpdatedWithoutHistoryFunction.java
@@ -7,15 +7,19 @@ import com.atlassian.jira.jql.query.QueryCreationContext;
 import com.atlassian.jira.jql.util.JqlDateSupport;
 import com.atlassian.jira.ofbiz.OfBizDelegator;
 import com.atlassian.jira.plugin.jql.function.AbstractJqlFunction;
+import com.atlassian.jira.plugin.jql.function.event.UpdatedByValidationEvent;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.MessageSet;
 import com.atlassian.jira.util.MessageSetImpl;
+import com.atlassian.jira.util.dbc.Assertions;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.query.clause.TerminalClause;
 import com.atlassian.query.operand.FunctionOperand;
 
 import javax.annotation.Nonnull;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @Scanned
@@ -24,9 +28,10 @@ public class UpdatedWithoutHistoryFunction extends AbstractJqlFunction {
     private final JqlDateSupport jqlDateSupport;
     private static final String ENTITY_ISSUE = "Issue";
 
-    public UpdatedWithoutHistoryFunction(OfBizDelegator ofBizDelegator, JqlDateSupport jqlDateSupport) {
-        this.ofBizDelegator = ofBizDelegator;
-        this.jqlDateSupport = jqlDateSupport;
+    public UpdatedWithoutHistoryFunction(@ComponentImport OfBizDelegator ofBizDelegator,
+                                         @ComponentImport JqlDateSupport jqlDateSupport) {
+        this.ofBizDelegator = Assertions.notNull("ofBizDelegator", ofBizDelegator);
+        this.jqlDateSupport = Assertions.notNull("jqlDateSupport", jqlDateSupport);
     }
 
     @Nonnull
@@ -39,6 +44,19 @@ public class UpdatedWithoutHistoryFunction extends AbstractJqlFunction {
             messageSet.addErrorMessage(this.getI18n().getText("jira.jql.function.anonymous.disallowed", this.getFunctionName()));
             return messageSet;
         } else if (args.size() <= 2) {
+            if (args.size() > 0) {
+                String dateFromString = args.get(0);
+                if (this.validateDate(messageSet, dateFromString, "jira.jql.function.updated.by.invalid.date.from") && args.size() > 1) {
+                    String dateToString = args.get(1);
+                    if (this.validateDate(messageSet, dateToString, "jira.jql.function.updated.by.invalid.date.to")) {
+                        Date dateFrom = this.jqlDateSupport.convertToDate(dateFromString);
+                        Date dateTo = this.jqlDateSupport.convertToDate(dateToString);
+                        if (dateTo.before(dateFrom)) {
+                            messageSet.addErrorMessage(this.getI18n().getText("jira.jql.function.updated.by.invalid.date.range", this.getFunctionName()));
+                        }
+                    }
+                }
+            }
             return messageSet;
         } else {
             messageSet.addErrorMessage(this.getI18n().getText("updated-without-history-function.invalid.argument.number", this.getFunctionName()));
