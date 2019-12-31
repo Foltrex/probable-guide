23f05c7f51920f08a87947dded1c7262a0345d95
Kalaputs, Andrei <akalaputs@scnsoft.com>
2019-12-31 17:44:36 +0300

    Logtime "Add Task" button refactoring.

--- scn-logtime-plugin/src/main/java/com/scn/jira/logtime/resource/LTUpdateScnWorklogsResource.java
+++ scn-logtime-plugin/src/main/java/com/scn/jira/logtime/resource/LTUpdateScnWorklogsResource.java
@@ -3,6 +3,7 @@ package com.scn.jira.logtime.resource;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
@@ -31,104 +32,101 @@ import com.scn.jira.logtime.store.ScnWorklogLogtimeStore;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
-/**
- * REST resource that provides a list of projects in JSON format.
- */
 @Named
 @Path("/updateScnWorklogs")
 public class LTUpdateScnWorklogsResource {
-	private IssueManager issueManager;
-	private IScnWorklogLogtimeStore iScnWorklogLogtimeStore;
+    private IssueManager issueManager;
+    private IScnWorklogLogtimeStore iScnWorklogLogtimeStore;
 
-	@Inject
-	public LTUpdateScnWorklogsResource(@ComponentImport IssueManager issueManager,
-			@ComponentImport ProjectRoleManager projectRoleManager,
-			@Qualifier("overridedWorklogManager") WorklogManager overridedWorklogManager,
-			@ComponentImport ScnProjectSettingsManager projectSettignsManager,
-			@ComponentImport ScnUserBlockingManager scnUserBlockingManager,
-			@ComponentImport DefaultScnWorklogService scnDefaultWorklogService) {
-		this.issueManager = issueManager;
-		this.iScnWorklogLogtimeStore = new ScnWorklogLogtimeStore(issueManager, projectRoleManager,
-				overridedWorklogManager, projectSettignsManager, scnUserBlockingManager, scnDefaultWorklogService);
-	}
+    @Inject
+    public LTUpdateScnWorklogsResource(@ComponentImport IssueManager issueManager,
+                                       @ComponentImport ProjectRoleManager projectRoleManager,
+                                       @Qualifier("overridedWorklogManager") WorklogManager overridedWorklogManager,
+                                       @ComponentImport ScnProjectSettingsManager projectSettignsManager,
+                                       @ComponentImport ScnUserBlockingManager scnUserBlockingManager,
+                                       @ComponentImport DefaultScnWorklogService scnDefaultWorklogService) {
+        this.issueManager = issueManager;
+        this.iScnWorklogLogtimeStore = new ScnWorklogLogtimeStore(issueManager, projectRoleManager,
+            overridedWorklogManager, projectSettignsManager, scnUserBlockingManager, scnDefaultWorklogService);
+    }
 
-	@GET
-	@AnonymousAllowed
-	@Produces({ "application/json", "application/xml" })
-	public Response getWorklogForUpdate(@Context HttpServletRequest request,
-			@QueryParam("wlsToSave") List<String> wlsToSave, @QueryParam("issueId") String issueId) {
-		if (wlsToSave == null)
-			return Response.ok("NOTHING TO SAVE").build();
+    @GET
+    @AnonymousAllowed
+    @Produces({"application/json", "application/xml"})
+    public Response getWorklogForUpdate(@Context HttpServletRequest request,
+                                        @QueryParam("wlsToSave") List<String> wlsToSave, @QueryParam("issueId") String issueId) {
+        if (wlsToSave == null)
+            return Response.ok("NOTHING TO SAVE").build();
 
-		Issue issue = issueManager.getIssueObject(Long.parseLong(issueId));
-		Map<String, String> wlToCreate = wlsToSave.stream()
-				.collect(Collectors.toMap(v -> getWlIdFromRequestParameter(v, 2), v -> v, (p, n) -> n)).entrySet()
-				.stream().filter(e -> !getWlIdFromRequestParameter(e.getValue(), 0).isEmpty())
-				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Issue issue = issueManager.getIssueObject(Long.parseLong(issueId));
+        Map<String, String> wlToCreate = wlsToSave.stream()
+            .collect(Collectors.toMap(v -> getWlIdFromRequestParameter(v, 2), v -> v, (p, n) -> n)).entrySet()
+            .stream().filter(e -> !getWlIdFromRequestParameter(e.getValue(), 0).isEmpty())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-		for (String wlKey : wlToCreate.keySet()) {
-			String wlString = wlToCreate.get(wlKey);
-			String time = getWlIdFromRequestParameter(wlString, 0);
-			String comment = String.valueOf(getWlIdFromRequestParameter(wlString, 1));
-			String date = getWlIdFromRequestParameter(wlString, 2);
-			String userKey = getWlIdFromRequestParameter(wlString, 3).equals("") ? null
-					: getWlIdFromRequestParameter(wlString, 3);
+        for (String wlKey : wlToCreate.keySet()) {
+            String wlString = wlToCreate.get(wlKey);
+            String time = getWlIdFromRequestParameter(wlString, 0);
+            String comment = String.valueOf(getWlIdFromRequestParameter(wlString, 1));
+            String date = getWlIdFromRequestParameter(wlString, 2);
+            String userKey = getWlIdFromRequestParameter(wlString, 3).equals("") ? null
+                : getWlIdFromRequestParameter(wlString, 3);
 
-			String worklogTypeId = String.valueOf(getWlIdFromRequestParameter(wlString, 4).equals("") ? ""
-					: Integer.valueOf(getWlIdFromRequestParameter(wlString, 4)));
+            String worklogTypeId = String.valueOf(getWlIdFromRequestParameter(wlString, 4).equals("") ? ""
+                : Integer.valueOf(getWlIdFromRequestParameter(wlString, 4)));
 
-			userKey = userKey != null ? userKey.toLowerCase() : "";
+            userKey = userKey != null ? userKey.toLowerCase() : "";
 
-			createWorklogS(time, comment, date, userKey, worklogTypeId, issueId);
-		}
+            createWorklogS(time, comment, date, userKey, worklogTypeId, issueId);
+        }
 
-		LTMessages message = new LTMessages("DONE!");
-		message.setMessage(issue != null ? String.valueOf(issue.getProjectObject().getId()) : "");
-		return Response.ok(message).build();
-	}
+        LTMessages message = new LTMessages("DONE!");
+        message.setMessage(issue != null ? String.valueOf(Objects.requireNonNull(issue.getProjectObject()).getId()) : "");
+        return Response.ok(message).build();
+    }
 
-	private void createWorklogS(String time, String comment, String date, String userKey, String worklogTypeId,
-			String issueId) {
-		Long timeSpent = 0L;
-		if (TextFormatUtil.matchesPattern1(time)) {
-			timeSpent = TextFormatUtil.stringToTime(time);
-		} else {
-			if (TextFormatUtil.matchesPattern2(time)) {
-				timeSpent = TextFormatUtil.string2ToTime(time);
-			} else {
-				if (TextFormatUtil.matchesPattern3(time)) {
-					timeSpent = TextFormatUtil.string3ToTime(time);
-				}
-			}
-		}
-		Date day = DateUtils.stringToDate(date);
-		if (day != null)
-			if (time != null && !time.equals("00:00") && !time.equals("0") && !time.equals(""))
-				// Long timeSpent = TextFormatUtil.stringToTime(time);
-				if (timeSpent.longValue() != 0)
-					// Here we will create a worklog
-					if (issueId != null && worklogTypeId != null)
-						createScnWorklog(Long.parseLong(issueId), null, worklogTypeId, timeSpent,
-								comment != null ? comment : "", userKey, day, String.valueOf(worklogTypeId));
-	}
+    private void createWorklogS(String time, String comment, String date, String userKey, String worklogTypeId,
+                                String issueId) {
+        Long timeSpent = 0L;
+        if (TextFormatUtil.matchesPattern1(time)) {
+            timeSpent = TextFormatUtil.stringToTime(time);
+        } else {
+            if (TextFormatUtil.matchesPattern2(time)) {
+                timeSpent = TextFormatUtil.string2ToTime(time);
+            } else {
+                if (TextFormatUtil.matchesPattern3(time)) {
+                    timeSpent = TextFormatUtil.string3ToTime(time);
+                }
+            }
+        }
+        Date day = DateUtils.stringToDate(date);
+        if (day != null)
+            if (time != null && !time.equals("00:00") && !time.equals("0") && !time.equals(""))
+                // Long timeSpent = TextFormatUtil.stringToTime(time);
+                if (timeSpent != 0)
+                    // Here we will create a worklog
+                    if (issueId != null && worklogTypeId != null)
+                        createScnWorklog(Long.parseLong(issueId), worklogTypeId, timeSpent,
+                            comment != null ? comment : "", userKey, day, worklogTypeId);
+    }
 
-	private boolean createScnWorklog(Long issueId, Long _worklogId, String _worklogType, Long _timeSpent,
-			String _comment, String authorKey, Date date, String worklogTypeId) {
-		return iScnWorklogLogtimeStore.createScnWorklog(issueId, _worklogType, _timeSpent, _comment, authorKey, date,
-				worklogTypeId);
-	}
+    private void createScnWorklog(Long issueId, String _worklogType, Long _timeSpent,
+                                  String _comment, String authorKey, Date date, String worklogTypeId) {
+        iScnWorklogLogtimeStore.createScnWorklog(issueId, _worklogType, _timeSpent, _comment, authorKey, date,
+            worklogTypeId);
+    }
 
-	public String getWlIdFromRequestParameter(String identifier, int i) {
-		// TESS-1_10000_0_08-01_143
-		// 2-wlid
-		// 3-date
-		// 1-wlTypeId
-		// 0-issueId
-		if (identifier != null && identifier.contains("_")) {
-			String[] arr = identifier.split("_");
-			return arr[i];
-		} else {
-			return "";
-		}
-	}
+    public String getWlIdFromRequestParameter(String identifier, int i) {
+        // TESS-1_10000_0_08-01_143
+        // 2-wlid
+        // 3-date
+        // 1-wlTypeId
+        // 0-issueId
+        if (identifier != null && identifier.contains("_")) {
+            String[] arr = identifier.split("_");
+            return arr[i];
+        } else {
+            return "";
+        }
+    }
 }
