8ff4a5e1be1b92588b5b3a33eb8dbe9daa49b2bd
Kalaputs, Andrei <akalaputs@scnsoft.com>
2020-02-06 00:46:32 +0300

    Optimized ext. worklog manager.

--- scn-worklog-plugin/src/main/java/com/scn/jira/worklog/core/wl/ExtendedWorklogManager.java
+++ scn-worklog-plugin/src/main/java/com/scn/jira/worklog/core/wl/ExtendedWorklogManager.java
@@ -8,13 +8,13 @@ import com.atlassian.jira.exception.DataAccessException;
 import com.atlassian.jira.issue.worklog.Worklog;
 
 public interface ExtendedWorklogManager {
-	public Worklog createExtWorklogType(Worklog worklog, String _worklogTypeId) throws DataAccessException;
+    Worklog createExtWorklogType(Worklog worklog, String _worklogTypeId) throws DataAccessException;
 
-	public List<GenericValue> getExtWorklogsByType(String worklogTypeId);
+    List<GenericValue> getExtWorklogsByType(String worklogTypeId);
 
-	public void updateExtWorklogType(Long _worklogId, String _worklogType) throws DataAccessException;
+    void updateExtWorklogType(Long _worklogId, String _worklogType) throws DataAccessException;
 
-	public GenericValue getExtWorklog(Long _worklogId) throws DataAccessException;
+    GenericValue getExtWorklog(Long _worklogId) throws DataAccessException;
 
-	public boolean deleteExtWorklogType(Long worklogId) throws DataAccessException;
+    boolean deleteExtWorklogType(Long worklogId) throws DataAccessException;
 }
