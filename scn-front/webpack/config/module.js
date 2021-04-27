5278d81f863cab6de7f199f63981b74821bc9861
Kalaputs, Andrei <akalaputs@scnsoft.com>
2021-04-27 09:30:49 +0300

    SCNATLASSIANDEV-52 - frontend refactoring.

---
+++ scn-front/webpack/config/module.js
@@ -0,0 +1,15 @@
+module.exports = {
+    rules: [
+        {
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+            },
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+        },
+    ],
+};
