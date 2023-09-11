0eadfb41b50cf3aa412aabd4520da8784a359628
Kalaputs, Andrei <akalaputs@scnsoft.com>
2023-09-11 12:55:56 +0300

    Revert "SCNATLASSIANDEV-86 - new ao model init"
    This reverts commit 9aeae66f06815c734c9ac80b0e1f189fc6499d01.

--- common/src/main/java/com/scn/jira/common/ao/Updatable.java
+++ common/src/main/java/com/scn/jira/common/ao/Updatable.java
@@ -1,26 +1,20 @@
 package com.scn.jira.common.ao;
 
 import java.sql.Timestamp;
-import net.java.ao.schema.NotNull;
 
 public interface Updatable extends WithId {
-
-    @NotNull
     String getAuthorKey();
 
     void setAuthorKey(String authorKey);
 
-    @NotNull
     String getUpdateAuthorKey();
 
     void setUpdateAuthorKey(String updateAuthorKey);
 
-    @NotNull
     Timestamp getCreated();
 
     void setCreated(Timestamp created);
 
-    @NotNull
     Timestamp getUpdated();
 
     void setUpdated(Timestamp updated);
