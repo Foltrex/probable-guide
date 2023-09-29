3867004e42d68ee98957d06de0beeddc9c6fe2e0
Foltrex <fpm.chernenkAV@bsu.by>
2023-09-29 14:30:51 +0300

    Add disabling/enabling jira core plugins modules

--- scn-worklog-plugin/src/main/java/com/scn/jira/worklog/listener/WorklogPluginLauncher.java
+++ scn-worklog-plugin/src/main/java/com/scn/jira/worklog/listener/WorklogPluginLauncher.java
@@ -9,6 +9,7 @@ import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.sal.api.lifecycle.LifecycleAware;
 import com.scn.jira.worklog.api.WorklogPluginComponent;
 import com.scn.jira.worklog.core.settings.IScnProjectSettingsManager;
+import com.scn.jira.worklog.utils.PluginManagerUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.apache.commons.lang.StringUtils;
@@ -45,14 +46,17 @@ public class WorklogPluginLauncher implements LifecycleAware {
     private final WorklogPluginComponent worklogPluginComponent;
     private final OfBizConnectionFactory ofBizConnectionFactory = DefaultOfBizConnectionFactory.getInstance();
     private final OfBizDelegator delegator;
+    private final PluginManagerUtils pluginManagerUtils;
 
     @Override
     public void onStart() {
+        pluginManagerUtils.disableSystemPlugins();
         log.warn(worklogPluginComponent.getName() + " has been started.");
     }
 
     @Override
     public void onStop() {
+        pluginManagerUtils.enableSystemPlugins();
     }
 
     private void migrateIncorrectScnWorklogs() {
